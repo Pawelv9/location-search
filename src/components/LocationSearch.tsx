2351ac236da81aa1f15d5d29e7749b3546df413c
Pavel <pawelv9@gmail.com>
2024-08-06 19:43:56 +0300

    added Form Submission

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,15 +1,34 @@
 import type { Place } from '../api/Place'
-import {useState} from "react";
+import React, {useState} from "react";
 
 interface LocationSearchProps {
     onPlaceClick: (place: Place) => void;
 }
 
 function LocationSearch({onPlaceClick}: LocationSearchProps): JSX.Element {
+    const [term, setTerm] = useState('')
     const [places, setPlaces] = useState<Place[]>([])
 
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        console.log('Needed to search an api..', term)
+    }
+
     return (
-        <div>Search for a locat`ion</div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label className="form-bold">Place
+                    Search
+                </label>
+            </form>
+            <input
+                className='border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full'
+                id='term'
+                value={term}
+                onChange={e => setTerm(e.target.value)}
+            />
+        </div>
     );
 }
 
