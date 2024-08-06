ef599ee0f7a5dd1675698ce4adad2a78d15cb01d
Pavel <pawelv9@gmail.com>
2024-08-06 19:23:47 +0300

    updated App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import LocationSearch from "./components/LocationSearch.tsx";
 import type { Place } from './api/Place'
+import {useState} from "react";
+
 function App() {
+    const [place, setPlace] = useState(Place | null)
 
   return (
     <div className="h-screen w-screen grid grid-cols-12">
@@ -8,7 +11,7 @@ function App() {
             <LocationSearch />
         </div>
         <div className="col-span-9">
-            <Map />
+            <Map place={place} />
         </div>
     </div>
   )
